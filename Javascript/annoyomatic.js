5e3996abf20ede5aa13f67557598ffe8ffd8e882
Kunal Singh <kunal1998doon@gmail.com>
2017-07-12 15:55:50 +0530

    Add files via upload

---
+++ Javascript/annoyomatic.js
@@ -0,0 +1,6 @@
+var s="";
+while(s!="yes"&&s!="yeah")
+{
+	s=prompt("are we there yet");
+}
+alert("we finally maade it");
---
+++ Javascript/whikeps.js
@@ -0,0 +1,37 @@
+/*------1
+var a=-10;
+while(a<=19)
+{
+	console.log(a);
+	a++;
+}
+*/
+
+/* 2
+var a=10;
+while(a<=40)
+{
+	console.log(a);
+	a=a+2;
+}
+*/
+/*
+var a=300;
+while(a<=333)
+{
+	if(a%2!=0)
+	{
+		console.log(a);
+	}
+	a++;
+}
+*/
+var a= 5;
+while(a<=50)
+{
+	if(a%3==0&&a%5==0)
+	{
+		console.log(a);
+	}
+	a++;
+}
